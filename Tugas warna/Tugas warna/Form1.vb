d3885bdd3d9b311aa11d3e5a84073da6a83664c6
achmadfaris01 <achmad.fariiz@gmail.com>
2015-10-01 17:23:41 +0700

    Latihan

---
+++ Tugas warna/Tugas warna/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.RoyalBlue Then
+            Me.BackColor = Color.Orange
+        Else
+            Me.BackColor = Color.RoyalBlue
+        End If
+    End Sub
+End Class
